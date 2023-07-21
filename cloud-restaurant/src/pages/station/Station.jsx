37a67d681b26b2bb2d33b44c6d4925c4bc2a0fd0
the wiki <olusegundavids1@gmail.com>
2023-07-20 22:57:02 -0800

    On branch master

--- cloud-restaurant/src/pages/station/Station.jsx
+++ cloud-restaurant/src/pages/station/Station.jsx
@@ -3,8 +3,34 @@ import './station .css';
 
 const Station = () => {
   return (
-    <div>
-      
+    <div className='container station_container'>
+      <article className='station_inner_container'>
+        <div className='station one'>
+          <p>Station 1</p>
+
+        </div>
+      </article>
+        <article className='station_inner_container'>
+          <div className='station one'>
+            <p>Station 1</p>
+
+          </div>
+        </article>
+          <article className='station_inner_container'>
+            <div className='station one'>
+              <p>Station 1</p>
+                </div>
+          </article>
+            <article className='station_inner_container'>
+              <div className='station one'>
+                <p>Station 1</p>
+                  </div>
+            </article>
+              <article className='station_inner_container'>
+               <div className='station one'>
+                <p>Station 1</p>
+               </div>
+              </article>
     </div>
   )
 }
