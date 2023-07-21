d1f9ee4c12f87cf689d15e80addf61e2b33cc327
the wiki <olusegundavids1@gmail.com>
2023-07-21 12:22:19 -0800

    On branch master

--- cloud-restaurant/src/components/nav/nav.jsx
+++ cloud-restaurant/src/components/nav/nav.jsx
@@ -5,18 +5,25 @@ import {LiaSignInAltSolid} from 'react-icons/lia';
 
 const Nav = () => {
   return (
-    <nav className='container nav-container'>
-      <Link to="/" className='logo'><p><span className='o'>O</span>food.</p></Link>
+    <nav className='container nav_container'>
+      <div className='nav_inner_container'>
+        <Link to="/" className='logo'><p><span className='o'>O</span>food.</p></Link>
       
-        <div className='navItems'> 
-          <Link to='/orders' className='link'> Orders </Link>
-          <Link to='/station' className='link'> Station </Link>
-          <Link to='/stocks' className='link'> Stock </Link>
-        </div>
+          <div className='navItems'> 
+            <Link to='/orders' className='link'> Orders </Link>
+            <Link to='/station' className='link'> Station </Link>
+            <Link to='/stocks' className='link'> Stock </Link>
+          </div>
 
-        <div>
-          <Link to='signup' className='sign-up'><LiaSignInAltSolid/></Link>
-        </div>
+          <div>
+            <Link to='signup' className='sign_up'><LiaSignInAltSolid/></Link>
+          </div>
+      </div>
+        <article className='body'>
+          <p>Welcome to <span>O</span>food.</p>
+          <p>can we get your order?</p>
+        </article>
+        
     </nav>
   )
 }
