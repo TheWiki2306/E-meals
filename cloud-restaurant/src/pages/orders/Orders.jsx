0df330b2828681cff6df69cd7d9eceefaa9b0cff
the wiki <olusegundavids1@gmail.com>
2023-08-02 14:58:56 -0800

    On branch master

--- cloud-restaurant/src/pages/orders/Orders.jsx
+++ cloud-restaurant/src/pages/orders/Orders.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { BiFoodMenu } from 'react-icons/bi';
+import { GiOpenedFoodCan } from 'react-icons/gi';
+import { PiBowlFoodLight } from 'react-icons/pi';
+import { MdFastfood } from 'react-icons/md';
+
 import './orders.css';
 
 const Orders = () => {
   return (
     <div>
-      
+      <p>Restaurants</p>
+      <div>
+        <article>
+          <p>{BiFoodMenu}</p>
+          <h4>French fry restaurant</h4>
+          <small>Ikeja, Lagos State.</small>
+        </article>
+        <article>
+          <p>{GiOpenedFoodCan}</p>
+          <h4>M&P restaurant</h4>
+          <small>Ikeja, Lagos State.</small>
+        </article>
+        <article>
+          <p>{PiBowlFoodLight}</p>
+          <h4>Mark Pasta</h4>
+          <small>Ikeja, Lagos State.</small>
+        </article>
+        <article>
+          <p>{MdFastfood}</p>
+          <h4>Daily paper restaurant</h4>
+          <small>Ikeja, Lagos State.</small>
+        </article>
+      </div>
     </div>
   )
 }
