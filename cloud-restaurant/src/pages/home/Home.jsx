89509d713eacec38ee3d17cb60c76e33b688c998
the wiki <olusegundavids1@gmail.com>
2023-08-04 11:52:29 -0800

    On branch master

--- cloud-restaurant/src/pages/home/Home.jsx
+++ cloud-restaurant/src/pages/home/Home.jsx
@@ -7,8 +7,27 @@ const Home = () => {
         <div className='body'>
           <p className='welcome'>Welcome to <span className='o'>O</span>food.</p>
           <p className='request'>can we get your order?</p>
-      
-          {/* every food you need and more */}
+
+          <div className='how'>
+            <h4>How it Works</h4>
+            <div className='how_container'>
+              <article className='location'>
+                <i></i>
+                <h5>Set delivery location</h5>
+                <small>Set the location for your delivery to be made</small>
+              </article>
+              <article className='products'>
+                <i></i>
+                <h5>Choose the product</h5>
+                <small>Set the location for your delivery to be made</small>
+              </article>
+              <article className='delivery'>
+                <i></i>
+                <h5>Receive it at your doorstep</h5>
+                <small>Set the location for your delivery to be made</small>
+              </article>
+            </div>
+          </div>
         </div>
     </div>
   )
