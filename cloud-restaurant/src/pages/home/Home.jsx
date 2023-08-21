b2f95fd278460e408935976c7b4d481118fa703f
the wiki <olusegundavids1@gmail.com>
2023-08-21 06:51:32 +0100

    On branch master

--- cloud-restaurant/src/pages/home/Home.jsx
+++ cloud-restaurant/src/pages/home/Home.jsx
@@ -15,32 +15,32 @@ const Home = () => {
         <div className='container body'>
           <p className='welcome'>Welcome to <span className='o'>O</span>food.</p>
           <p className='request'>can we get your order?</p>
-          <div>
-            <Link to='/orders' className='link'> Orders </Link>
-          </div>
         </div>
-          <div className='how'>
-            <h4>How it Works</h4>
-            <div className='how_container'>
-              <article className='location'>
-                <i><GrLocation /></i>
-                <h5>Set delivery location</h5>
-                <small>Set the location for your delivery to be made</small>
-              </article>
-              <article className='products'>
-                <i><MdProductionQuantityLimits /></i>
-                <h5>Choose the product</h5>
-                <small>Set the location for your delivery to be made</small>
-              </article>
-              <article className='delivery'>
-                <i><TbTruckDelivery /></i>
-                <h5>Receive it at your doorstep</h5>
-                <small>Set the location for your delivery to be made</small>
-              </article>
-            </div>
+          <div className='orders'>
+              <Link to='/orders' className='link'> Orders </Link>
           </div>
-          <Vendor />
-        {/* </div> */}
+            <div className='how'>
+              <h4>How it Works</h4>
+              <div className='how_container'>
+                <article className='location'>
+                  <i><GrLocation /></i>
+                  <h5>Set delivery location</h5>
+                  <small>Set the location for your delivery to be made</small>
+                </article>
+                <article className='products'>
+                  <i><MdProductionQuantityLimits /></i>
+                  <h5>Choose the product</h5>
+                  <small>Set the location for your delivery to be made</small>
+                </article>
+                <article className='delivery'>
+                  <i><TbTruckDelivery /></i>
+                  <h5>Receive it at your doorstep</h5>
+                  <small>Set the location for your delivery to be made</small>
+                </article>
+              </div>
+            </div>
+            <Vendor />
+          {/* </div> */}
     </div>
      <Routes>
      <Route path='/vendor_signup' element={<VendorSignup />}/>
